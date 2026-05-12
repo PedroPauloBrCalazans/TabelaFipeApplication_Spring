cacc270d0771268bc36a325ec9b1bca21eb78f30
PedroPauloBrCalazans <pedropaulo91.calazans@gmail.com>
2026-05-11 23:58:02 -0300

    FEAT: Implementação da classe de dados + método para obterlista.

--- src/main/java/br/com/bulltech/TabelaFipe/service/ConverteDados.java
+++ src/main/java/br/com/bulltech/TabelaFipe/service/ConverteDados.java
@@ -2,6 +2,9 @@ package br.com.bulltech.TabelaFipe.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+
+import java.util.List;
 
 public class ConverteDados implements IConverteDados {
 
@@ -11,6 +14,18 @@ public class ConverteDados implements IConverteDados {
     public <T> T obterDados(String json, Class<T> classe) {
         try{
             return mapper.readValue(json, classe);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public <T> List<T> obterLista(String json, Class<T> classe) {
+        CollectionType lista = mapper.getTypeFactory()
+                .constructCollectionType(List.class, classe); // construir uma lista na base no que vamos passar
+
+        try {
+            return mapper.readValue(json, lista);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
