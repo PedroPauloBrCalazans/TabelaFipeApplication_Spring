5393cef1f0526f3563aadee89ff2c4b7b2eb6d7e
PedroPauloBrCalazans <pedropaulo91.calazans@gmail.com>
2026-05-16 01:21:57 -0300

    FEAT: Implementação da classe de modelos + fluxo para escolha do modelo.

--- src/main/java/br/com/bulltech/TabelaFipe/principal/Principal.java
+++ src/main/java/br/com/bulltech/TabelaFipe/principal/Principal.java
@@ -1,13 +1,18 @@
 package br.com.bulltech.TabelaFipe.principal;
 
+import br.com.bulltech.TabelaFipe.model.Dados;
+import br.com.bulltech.TabelaFipe.model.Modelos;
 import br.com.bulltech.TabelaFipe.service.ConsumoAPI;
+import br.com.bulltech.TabelaFipe.service.ConverteDados;
 
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Principal {
 
     private Scanner leitura = new Scanner(System.in);
     private ConsumoAPI consumoAPI = new ConsumoAPI();
+    private ConverteDados conversor = new ConverteDados();
 
     private final String URL_BASE = "https://parallelum.com.br/fipe/api/v1/";
 
@@ -34,8 +39,21 @@ public class Principal {
         }
 
         var json = consumoAPI.obterDados(endereco);
-
         System.out.println(json);
+        var marcas = conversor.obterLista(json, Dados.class);
+        marcas.stream()
+                .sorted(Comparator.comparing(Dados::codigo))
+                .forEach(System.out::println);
 
+        System.out.println("Informe o código da marca para consulta: ");
+        var codigoMarca = leitura.nextLine();
+
+        endereco = endereco + "/" + codigoMarca + "/modelos";
+        json = consumoAPI.obterDados(endereco);
+        var modeloLista = conversor.obterDados(json, Modelos.class);
+
+        System.out.println("\nModelos dessa marca: ");
+        modeloLista.modelos().stream()
+                .sorted(Comparator.comparing(Dados::codigo)).forEach(System.out::println);
     }
 }
